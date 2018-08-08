fc2c5f0c059e82adbe880da5a5f57a609edfda31
Ahmad Ajmi <ahmad@ahmadajmi.com>
2018-08-08 14:19:26 +0200

    1.0.3 Fixed and redesigned search issues Fixed content width in small screens

--- js/app.js
+++ js/app.js
@@ -42,12 +42,14 @@ $(document).ready(function() {
   // =====================
 
   var search_field = $('.js-search-input'),
-      search_results = $('.js-search-result'),
+      search_results = $('.js-search-results'),
       toggle_search = $('.js-search-toggle'),
       search_result_template = "\
-        <div class='c-search-result__item'>\
-          <a class='c-search-result__title' href='{{link}}'>{{title}}</a>\
-        </div>";
+      <a href='{{link}}' class='c-search-result'>\
+        <div class='c-search-result__content'>\
+          <h3 class='c-search-result__title'>{{title}}</h3>\
+        </div>\
+      </a>";
 
   toggle_search.click(function(e) {
     e.preventDefault();
@@ -61,19 +63,20 @@ $(document).ready(function() {
     }, 500);
   });
 
-  $('.c-search, .js-search-close').on('click keyup', function(event) {
-    if (event.target == this || event.target.className == 'js-search-close' || event.keyCode == 27) {
+  $('.c-search, .js-search-close, .js-search-close .icon').on('click keyup', function(event) {
+    if (event.target == this || event.target.className == 'js-search-close' || event.target.className == 'icon' || event.keyCode == 27) {
       $('.c-search').removeClass('is-active');
     }
   });
 
   search_field.ghostHunter({
     results: search_results,
-    onKeyUp         : true,
-    rss             : base_url + '/feed.xml',
-    info_template   : "<h4 class='c-search-result__head'>Number of results found: {{amount}}</h4>",
+    onKeyUp: true,
+    rss: base_url + '/feed.xml',
     result_template : search_result_template,
     zeroResultsInfo : false,
+    displaySearchInfo: false,
+    includepages : true,
     before: function() {
       search_results.fadeIn();
     }
